2a2d9f09ad5c543d8c36463cb1464570b9eabca4
Mushrumt <mushrumt02@gmail.com>
2024-05-23 18:35:24 +0300

    Добавьте файлы проекта.

---
+++ LibraryController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ASP_LR_4_Kyrylenko_402
+{
+    public class LibraryController : Controller
+    {
+        [Route("Library")]
+        public IActionResult Library()
+        {
+            return Content("You are in a library");
+        }
+
+        [Route("Library/Books")]
+        public IActionResult Books()
+        {
+            var books = ReadBooksFromJson();
+            return Json(books);
+        }
+
+        [Route("Library/Profile/{id?}")]
+        public IActionResult Profile(int? id)
+        {
+            List<User> users = ReadUsersFromJson();
+
+            if (id.HasValue && id >= 0 && id <= 5)
+            {
+                var user = users.Find(u => u.Id == id);
+                if (user != null)
+                    return Json(user);
+                else
+                    return NotFound();
+            }
+            else
+            {
+                return Content("current user info: artem");
+            }
+        }
+
+        private List<Book> ReadBooksFromJson()
+        {
+            var json = System.IO.File.ReadAllText("books.json");
+            return JsonConvert.DeserializeObject<List<Book>>(json);
+        }
+
+        private List<User> ReadUsersFromJson()
+        {
+            var json = System.IO.File.ReadAllText("users.json");
+            return JsonConvert.DeserializeObject<List<User>>(json);
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,57 @@
+﻿namespace ASP_LR_4_Kyrylenko_402
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "library",
+                    pattern: "Library",
+                    defaults: new { controller = "Library", action = "Index" });
+
+                endpoints.MapControllerRoute(
+                    name: "library_books",
+                    pattern: "Library/Books",
+                    defaults: new { controller = "Library", action = "Books" });
+
+                endpoints.MapControllerRoute(
+                    name: "library_profile",
+                    pattern: "Library/Profile/{id?}",
+                    defaults: new { controller = "Library", action = "Profile" });
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
